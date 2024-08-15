36d96cf549e62543a90ad4e99279b8dde611e1c7
SeongHwan <34639547+osh0678@users.noreply.github.com>
2024-08-15 21:52:29 +0900

    feat: time zone setting

--- AppleClock/AppleClock/WorldClockViewController.swift
+++ AppleClock/AppleClock/WorldClockViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class WorldClockViewController: UIViewController {
 
+    @IBOutlet weak var worldClockTableView: UITableView!
+    
+    var list = [
+        TimeZone(identifier: "Asia/Seoul")!,
+        TimeZone(identifier: "Europe/Paris")!
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+}
+
+extension WorldClockViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: WorldClockTableViewCell.self), for: indexPath) as! WorldClockTableViewCell
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
