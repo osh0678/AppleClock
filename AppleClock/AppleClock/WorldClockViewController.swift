ed2cfd4323831b23a1c151fc77c1d3d4849b0316
SeongHwan <34639547+osh0678@users.noreply.github.com>
2024-08-24 16:29:34 +0900

    feat: duplicate timezone cant list add

--- AppleClock/AppleClock/WorldClockViewController.swift
+++ AppleClock/AppleClock/WorldClockViewController.swift
@@ -22,7 +22,19 @@ class WorldClockViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        NotificationCenter.default.addObserver(forName: .timeZoneDidSelect, object: nil, 
+                                               queue: .main) { [weak self] noti in
+            guard let self, let timeZone = noti.userInfo?["timeZone"] as? TimeZone else {
+                return
+            }
+            
+            guard !self.list.contains(where: { $0.identifier == timeZone.identifier }) else {
+                return
+            }
+                    
+            self.list.append(timeZone)
+            self.worldClockTableView.reloadData()
+        }
     }
 
 }
